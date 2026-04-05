2bddf5eb735f9aaaec781ca9b05f4cb929fa75d6
sivakavihemalatha-hub <sivakavihemalatha@gmail.com>
2026-04-05 14:57:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -294,4 +294,5 @@ def logout():
 
 # ---------------- RUN ----------------
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
